fed358f4349d4864a209bdc6c5305fb9eb16090d
Katrina Pierre <kpierre@broadinstitute.org>
2022-08-16 18:11:56 -0400

    Maps/Sets exercise

---
+++ ES2015/maps-sets-exercises.js
@@ -0,0 +1,33 @@
+// QQ1
+const a1 = "[1,2,3,4]"
+
+// QQ2
+const a2 = "'ref'"
+
+//QQ3
+const a3 = `
+{ Array(3) => true }
+{ Array(3) => false }`
+
+// hasDuplicate()
+const hasDuplicate = arr => new Set(arr).size !== arr.length
+
+// vowelCount
+function isVowel(char){
+  return "aeiou".includes(char);
+}
+
+function vowelCount(str){
+  const vowelMap = new Map();
+  for(let char of str){
+    let lowerCaseChar = char.toLowerCase()
+    if(isVowel(lowerCaseChar)){
+      if(vowelMap.has(lowerCaseChar)){
+        vowelMap.set(lowerCaseChar, vowelMap.get(lowerCaseChar) + 1);
+      } else {
+        vowelMap.set(lowerCaseChar, 1);
+      }
+    }
+  }
+  return vowelMap;
+}
