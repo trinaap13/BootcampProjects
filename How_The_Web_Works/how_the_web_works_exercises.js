7deb3be0ee8cf067d828a2b6efbc5895f4dcc055
Katrina Pierre <kpierre@broadinstitute.org>
2022-09-20 00:02:35 -0400

    How the web works exercises

---
+++ How_The_Web_Works/how_the_web_works_exercises.js
@@ -0,0 +1,12 @@
+//PART ONE
+// What is HTTP?
+var a1 = "Hyper Text Transfer Protocol"
+// What is a URL?
+var a2 = "Uniform Resource Locator; an address for an internet resource"
+// What is DNS?
+// What is a query string?
+// What are two HTTP verbs and how are they different?
+// What is an HTTP request?
+// What is an HTTP response?
+// What is an HTTP header? Give a couple examples of request and response headers you have seen.
+// What are the processes that happen when you type “http://somesite.com/some/page.html” into a browser?
