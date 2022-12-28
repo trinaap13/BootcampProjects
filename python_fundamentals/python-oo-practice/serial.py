7b4ed242fde958d4fda9b4636db269dd7af2afde
Katrina Pierre <katrina.pierre13@gmail.com>
2022-12-28 16:59:14 -0500

    serial and wordfinder practice files

--- python_fundamentals/python-oo-practice/serial.py
+++ python_fundamentals/python-oo-practice/serial.py
@@ -20,3 +20,24 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start=0):
+        """New generator with initial start of 0"""
+
+        self.start = self.next = start
+
+    def __repr__(self):
+        """Representation of class"""
+
+        return f"<SerialGenerator start={self.start} next={self.next}>"
+
+    def generate(self):
+        """Return next number"""
+
+        self.next += 1
+        return self.next - 1
+
+    def reset(self):
+        """Reset to start"""
+
+        self.next = self.start
+
