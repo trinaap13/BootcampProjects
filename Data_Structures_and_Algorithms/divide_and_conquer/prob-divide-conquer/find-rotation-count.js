5da81cd6d60a4976319273d406ba81ffff59a555
Katrina Pierre <katrina.pierre13@gmail.com>
2023-06-29 15:09:31 -0400

    find rotation count

--- Data_Structures_and_Algorithms/divide_and_conquer/prob-divide-conquer/find-rotation-count.js
+++ Data_Structures_and_Algorithms/divide_and_conquer/prob-divide-conquer/find-rotation-count.js
@@ -1,5 +1,20 @@
-function findRotationCount() {
+function findRotationCount(arr, low = 0, high = arr.length - 1) {
+    if (high < low) return 0;
+    if (high === low) return low;
+    let mid = Math.floor((low + high) / 2)
   
-}
+    if (mid < high && arr[mid + 1] < arr[mid])
+      return mid + 1;
+  
+    if (mid > low && arr[mid] < arr[mid - 1]) {
+      return mid;
+    }
 
-module.exports = findRotationCount
+    if (arr[high] > arr[mid]) {
+      return findRotationCount(arr, low, mid - 1);
+    }
+  
+    return findRotationCount(arr, mid + 1, high);
+  }
+  
+  module.exports = findRotationCount
